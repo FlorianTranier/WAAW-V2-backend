978274e2b19c09c43413bc3377e3141cdc04c469
Blanymon <tranierflorian@gmail.com>
2021-11-13 15:47:06 +0100

    [chore] add eslint rules

--- src/domain/services/audio/AudioService.ts
+++ src/domain/services/audio/AudioService.ts
@@ -2,14 +2,14 @@ import { Readable } from 'stream'
 import { IAudioProvider } from '@/domain/providers/audio/IAudioProvider'
 export class AudioService {
 
-	private readonly audioProvider: IAudioProvider
+  private readonly audioProvider: IAudioProvider
 
-	constructor(p: { audioProvider: IAudioProvider }) {
-		this.audioProvider = p.audioProvider
-	}
+  constructor(p: { audioProvider: IAudioProvider }) {
+    this.audioProvider = p.audioProvider
+  }
 
-	async getAudioStreamById(id: string): Promise<Readable> {
-		return this.audioProvider.getAudioStreamById(id)
-	}
+  async getAudioStreamById(id: string): Promise<Readable> {
+    return this.audioProvider.getAudioStreamById(id)
+  }
 
 }
